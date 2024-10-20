0dee83e3c6f7fab6ce1b0fcdf3bdfd1aa2a7c7bc
maloe19 <nartim087@gmail.com>
2024-10-20 02:27:57 +0200

    removed_react_website

--- my-react-app/src/pages/Error.js
+++
@@ -1,7 +0,0 @@
-const Error = () => {
-    return <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-      <h1>404 not found</h1>
-    </div>;
-  };
-  
-  export default Error;
